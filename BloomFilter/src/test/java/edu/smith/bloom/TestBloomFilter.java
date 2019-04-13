0a223453caff99ea229d3f1269c096ea53fd3498
sivannachum <43347661+sivannachum@users.noreply.github.com>
2019-04-12 21:25:47 -0400

    Break

---
+++ BloomFilter/src/test/java/edu/smith/bloom/TestBloomFilter.java
@@ -0,0 +1,12 @@
+package edu.smith.bloom;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestBloomFilter {
+	@Test
+	public void testEmpty() {
+		BloomFilter<String> bloom = new BloomFilter<String>(10, 2);
+		//for ()
+	}
+}
